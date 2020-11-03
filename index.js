32a2284f35a7b1479ac02fdf235d0c53d0206a2b
abosen <abosen@qq.com>
2020-11-04 00:25:29 +0800

    3.2

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ let persons = [
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
+app.get('/info', (req, res) => {
+    res.send(`<div>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>
+</div>`)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
